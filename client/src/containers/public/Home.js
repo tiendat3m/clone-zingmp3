fb146a9612e3a6a923cff73bbcb46c43bf41b824
unknown <you@example.com>
2024-02-28 15:14:51 +0700

    dat

--- client/src/containers/public/Home.js
+++ client/src/containers/public/Home.js
@@ -4,6 +4,8 @@ import { useSelector } from 'react-redux'
 import { Link } from "react-router-dom";
 import Sliders from 'react-slick'
 const Home = () => {
+  const { isLoading } = useSelector(state => state.app)
+
   const scrollRef = useRef()
   const settings = {
     dots: false,
@@ -19,49 +21,50 @@ const Home = () => {
   }, [])
   return (
     <>
-      {(top100 && weekChart && singers && chill && hotAlbum)
-        ? <div ref={scrollRef} className="w-full">
-          <div className="w-full h-[70px]"></div>
-          <Slider />
-          <NewRelease />
-          <Section data={favoritedArtists} isHideAllButton />
-          <ChartSection />
-          <div className="flex items-center px-[43px] w-full mt-12">
-            {weekChart?.map(item => (
-              <Link
-                to={item.link?.split(".")[0]} key={item.link}
-                className="flex-1 px-4"
-              >
-                <img src={item.cover} alt="cover" className="w-full object-cover rounded-md" />
-              </Link>
-            ))}
-          </div>
-          {singers && <div className={`px-[43px] w-full mt-12 `}>
-            <Sliders {...settings}>
-              {singers?.map(item => (
-                <div
-                  key={item.id}
-                  className='px-4'
-                >
-                  <Artist
-                    image={item.thumbnail}
-                    follower={item.totalFollow}
-                    link={item.link}
-                    title={item.name}
-                  />
-                </div>
-              ))}
-            </Sliders>
-          </div>}
-          <Section data={top100} />
-          <Section data={chill} />
-          <Section data={hotAlbum} />
-          <div className="px-[43px] w-full mt-12">
-            <Footer />
-          </div>
-          <div className="h-[120px]"></div>
+      <div ref={scrollRef} className="w-full">
+        {isLoading && <div className='absolute top-0 left-0 z-20 bottom-0 right-0 bg-sidebar flex items-center justify-center'>
+          <span><Loading /></span>
+        </div>}
+        <div className="w-full h-[70px]"></div>
+        <Slider />
+        <NewRelease />
+        <Section data={favoritedArtists} isHideAllButton />
+        <ChartSection />
+        <div className="flex items-center px-[43px] w-full mt-12">
+          {weekChart?.map(item => (
+            <Link
+              to={item.link?.split(".")[0]} key={item.link}
+              className="flex-1 px-4"
+            >
+              <img src={item.cover} alt="cover" className="w-full object-cover rounded-md" />
+            </Link>
+          ))}
         </div>
-        : <div className="w-full h-full flex items-center justify-center"><Loading /></div>}
+        {singers && <div className={`px-[43px] w-full mt-12 `}>
+          <Sliders {...settings}>
+            {singers?.map(item => (
+              <div
+                key={item.id}
+                className='px-4'
+              >
+                <Artist
+                  image={item.thumbnail}
+                  follower={item.totalFollow}
+                  link={item.link}
+                  title={item.name}
+                />
+              </div>
+            ))}
+          </Sliders>
+        </div>}
+        <Section data={top100} />
+        <Section data={chill} />
+        <Section data={hotAlbum} />
+        <div className="px-[43px] w-full mt-12">
+          <Footer />
+        </div>
+        <div className="h-[120px]"></div>
+      </div>
     </>
   );
 };
--- client/src/containers/public/Top100.js
+++ client/src/containers/public/Top100.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import * as actions from "../../store/actions";
-import { Section } from '../../components';
-import top100 from '../../assets/top100.PNG'
+import { Loading, Section } from '../../components';
 
 const Top100 = () => {
     const scrollRef = useRef()
@@ -15,12 +14,16 @@ const Top100 = () => {
     }, []);
 
     return (
-        <div ref={scrollRef} className='py-[100px]'>
-            <Section data={outstanding} isHideAllButton={true} />
-            <Section data={vnMusic} isHideAllButton={true} />
-            <Section data={asiaMusic} isHideAllButton={true} />
-            <Section data={usMusic} isHideAllButton={true} />
-            <Section data={concertMusic} isHideAllButton={true} />
+        <div ref={scrollRef}>
+            {(outstanding && vnMusic && asiaMusic && usMusic && concertMusic) ? <div className='py-[100px] flex flex-col relative flex-auto'>
+                <Section data={outstanding} isHideAllButton={true} />
+                <Section data={vnMusic} isHideAllButton={true} />
+                <Section data={asiaMusic} isHideAllButton={true} />
+                <Section data={usMusic} isHideAllButton={true} />
+                <Section data={concertMusic} isHideAllButton={true} />
+            </div> : <div className='absolute top-0 left-0 z-20 bottom-0 right-0 bg-layout flex items-center justify-center'>
+                <Loading />
+            </div>}
         </div>
     )
 }
